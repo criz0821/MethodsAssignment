55f79ad4ff8f02c69088b1203f1ebef95205a05b
Chris Mejia <cbmejia@usf.edu>
2019-01-31 21:31:44 -0500

    Version 1

--- MethodsAssignment/Program.cs
+++ MethodsAssignment/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+    Author: Christopher Mejia
+    Date: 1/31/19
+    Comments: This assignment demonstrates the use of a method
+    On another note... enter my name (Chris Mejia) into the console :)
+ */
+using System;
 
 namespace MethodsAssignment
 {
@@ -6,7 +12,35 @@ namespace MethodsAssignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+            Console.WriteLine("Please enter you full name");
+            string userInput = Console.ReadLine();
+                    
+            try
+            {
+                //calling custom method
+                Hello(userInput);
+                Console.ReadKey(true);
+            }// end of try
+
+            catch
+            {
+                Console.WriteLine("Sorry an error occured");
+                Console.ReadKey(true);
+            }// end of catch
+        }// end of main
+
+        private static void Hello(string name)
+        {
+            if (name == "Chris Mejia")
+            {
+                Console.WriteLine("Hello father, thanks for creating me!");
+            }// end of if statement
+
+            else
+            {
+                Console.WriteLine("Hello " + name + "! I hope your day is going well.");
+            }// end of else statement
+        }// end of custom method Hello
+
+    }// end of class
+}// end of namespace
